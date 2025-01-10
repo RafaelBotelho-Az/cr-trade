bc742d925d7fb9a6e0924c3afe0718f358209b69
Rafael Botelho <rafaelbotelho@live.com>
2025-01-10 20:09:58 -0300

    criação dos primeiros models

--- produto/models.py
+++ produto/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    nome = models.CharField(max_length=255)
+    descricao = models.TextField(max_length=255)
+    imagem = models.ImageField(upload_to='produto_imagens', blank=True, null=True)
+    slug = models.SlugField(unique=True)
+    preco_marketing = models.FloatField(default=0)
+    preco_marketing_promo = models.FloatField(default=0)
+    tipo = models.CharField(
+        default='V',
+        max_length=1,
+        choices=(
+            ('V', 'Variação'),
+            ('S', 'Simples')
+        )
+    )
+
+    def __str__(self):
+        return self.nome
+    
+class Variacao(models.Model):
+    produto = models.ForeignKey(Produto, on_delete=models.CASCADE)
+    nome = models.CharField(max_length=50, blank=True, null=True)
+    preco = models.FloatField(default=0)
+    preco_promo = models.FloatField(default=0)
+    estoque = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return self.nome or self.produto.nome
+    
+    class Meta:
+        verbose_name = 'Variação'
+        verbose_name_plural = 'Variações'
